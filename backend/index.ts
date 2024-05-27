691cbc39149a86aeb7c6b7c2c3c9a0ba889294d6
Daniel Souza <danielsouza51764@gmail.com>
2024-05-27 16:02:30 -0300

    first commit

---
+++ backend/index.ts
@@ -0,0 +1,23 @@
+import path from "path";
+import { buildSchema } from "type-graphql";
+import { ApolloServer } from "apollo-server";
+
+import "reflect-metadata";
+import { UserResolver } from "./src/resolvers/UserResolver";
+
+async function main() {
+  const schema = await buildSchema({
+    resolvers: [UserResolver],
+    emitSchemaFile: path.resolve(__dirname, "schema.gql"),
+  });
+
+  const server = new ApolloServer({
+    schema,
+  });
+
+  const { url } = await server.listen();
+
+  console.log(`Server running on ${url}`);
+}
+
+main();
